1f4f68b091e84044ce5d89ca60286b3257ac97bc
teofilorf <teofilo@rabelo.net.br>
2017-05-04 16:11:06 -0300

    001 remasterlinux

--- remasterlinux/remasterlinux.sh
+++ remasterlinux/remasterlinux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd tmp
-mkdir remaster
-wget -c https://sourceforge.net/projects/pinguy-os/files/ISO_Builder/pinguybuilder_4.3-8_all-beta.deb/download -O pinguybuilder.deb
-sudo dpkg -i pinguybuilder.deb
+diretorio="/tmp/remaster"
+mkdir -p $diretorio
+wget -c https://sourceforge.net/projects/pinguy-os/files/ISO_Builder/pinguybuilder_4.3-8_all-beta.deb/download -O $diretorio/pinguybuilder.deb
+sudo dpkg -i $diretorio/pinguybuilder.deb
 sudo apt-get install -f
