67c7d8449b4fc247631b1ed10c50d9c6893ac909
teofilorf <teofilo@rabelo.net>
2017-05-05 18:48:21 -0300

    quase pronto

--- jnunes/files/grdesktop.sh
+++ jnunes/files/grdesktop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 servidor=$(yad --title "Acesso Remoto" --text "Servidor" --entry)
-rdesktop -g 1360x700 $servidor
+rdesktop -d jotanunes -g 1360x700 $servidor
--- jnunes/provision.sh
+++ jnunes/provision.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
 #atulaizar
-#sudo apt update && sudo apt upgrade -y
+sudo apt update && sudo apt upgrade -y
 
 #adicionar repositorios
 sudo add-apt-repository -y ppa:emoraes25/cid && sudo apt-get update 
 
 #instalar pacotes
-#sudo apt install -y cups-pdf thunderbird thunderbird-locale-pt-br cid-base cid cid-gtk rdesktop
-sudo apt install -y cid-base cid cid-gtk
+sudo apt install -y cups-pdf cid-base cid cid-gtk rdesktop yad 
+sudo dpkg -i files/ampagent-6.3.314.ubuntu.64.deb
+sudo apt install -f
+
+#post install
+sudo mv /etc/mdm/mdm.conf /etc/mdm/mdm.conf.bkp
+sudo cp -f files/mdm.conf /etc/mdm/
+
+sudo cp files/linux-brprinter-installer-2.1.1-1 /usr/local/bin/lbrprinter
+sudo chmod +x /usr/local/bin/lbrprinter
+sudo cp files/Remote_desktop_connection_icon.png /usr/share/icons/
+sudo cp files/grdesktop.sh /usr/local/bin/
+sudo chmod +x /usr/local/bin/grdesktop.sh
+sudo rm -rf /etc/skel
+suco cp -rf files/skel /etc/
+
 #ingressar dominio
 sudo cid-gtk
-
-sudo reboot
