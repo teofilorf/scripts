c156674790207a8a7c84054b7ced312c3e543b04
teofilorf <teofilo@rabelo.net.br>
2017-05-08 12:58:24 -0300

    add ingress ad

--- ingressad/ingressad.sh
+++ ingressad/ingressad.sh
@@ -4,7 +4,7 @@
 sudo add-apt-repository -y ppa:emoraes25/cid && sudo apt-get update 
 
 #instalar pacotes
-sudo apt install -y cid-base cid cid-gtk
+sudo apt install -y cid-base cid
 
 #comando para ingressar
 #cid joindomain domain= user= pass=
