7499ae263d5bbffe19f725520ec1c2695ed9eba5
teofilorf <teofilo@rabelo.net.br>
2017-05-11 13:01:13 -0300

    123341

---
+++ atu.sh
@@ -0,0 +1,1 @@
+git add * && git commit -m 123341 && git push origin master
--- jnunes/provision.sh
+++ jnunes/provision.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-#atulaizar
-sudo apt update && sudo apt upgrade -y
 
 #adicionar repositorios
-sudo add-apt-repository -y ppa:emoraes25/cid && sudo apt-get update 
+sudo add-apt-repository -y ppa:webupd8team/java && sudo add-apt-repository -y ppa:emoraes25/cid && sudo apt-get update && sudo apt upgrade -y
 
 #instalar pacotes
-sudo apt install -y cups-pdf cid-base cid cid-gtk rdesktop yad 
+sudo apt install -y cups-pdf cid-base cid cid-gtk rdesktop yad oracle-java8-set-default 
 sudo dpkg -i files/ampagent-6.3.314.ubuntu.64.deb
-sudo apt install -f
+cd /tmp
+wget -c https://download.anydesk.com/linux/anydesk_2.9.0-1_amd64.deb?_ga=2.246307844.1450319514.1494517567-689502156.1494517567 -O anydesk.deb
+sudo dpkg -i anydesk.deb
+wget -c https://download.teamviewer.com/download/teamviewer_i386.deb
+sudo dpkg -i teamviewer_i386.deb
+sudo apt install -f -y
 
 #post install
 sudo mv /etc/mdm/mdm.conf /etc/mdm/mdm.conf.bkp
