10588bcddeeb8f6b92aa376a9214587ea8a1a160
teofilorf <teofilo@rabelo.net.br>
2017-05-11 13:30:31 -0300

    123341

--- jnunes/provision.sh
+++ jnunes/provision.sh
@@ -7,13 +7,11 @@ sudo add-apt-repository -y ppa:webupd8team/java && sudo add-apt-repository -y pp
 #instalar pacotes
 sudo apt install -y cups-pdf cid-base cid cid-gtk rdesktop yad oracle-java8-set-default 
 sudo dpkg -i files/ampagent-6.3.314.ubuntu.64.deb
-cd /tmp
 wget -c https://download.anydesk.com/linux/anydesk_2.9.0-1_amd64.deb?_ga=2.246307844.1450319514.1494517567-689502156.1494517567 -O anydesk.deb
 sudo dpkg -i anydesk.deb
 wget -c https://download.teamviewer.com/download/teamviewer_i386.deb
 sudo dpkg -i teamviewer_i386.deb
 sudo apt install -f -y
-
 #post install
 sudo mv /etc/mdm/mdm.conf /etc/mdm/mdm.conf.bkp
 sudo cp -f files/mdm.conf /etc/mdm/
