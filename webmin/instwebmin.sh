d3298ed6e13736c5c4421f5df577d553911e3ddb
teofilorf <teofilo@rabelo.net.br>
2017-05-15 17:06:23 -0300

    123341

--- webmin/instwebmin.sh
+++ webmin/instwebmin.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 echo "deb http://download.webmin.com/download/repository sarge contrib" > sudo /etc/apt/sources.list.d/webmin.list &&\
+wget http://www.webmin.com/jcameron-key.asc && \
+apt-key add jcameron-key.asc && \
 sudo apt update && \
 sudo apt install -y webmin
