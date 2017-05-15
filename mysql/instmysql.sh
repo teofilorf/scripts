4dbb11dcd381ca11f01de6a37e0ec77eb5e61474
teofilorf <teofilo@rabelo.net.br>
2017-05-15 15:15:04 -0300

    123341

---
+++ mysql/instmysql.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt -y install mysql-server-5.7
---
+++ webmin/instwebmin.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list &&\
+sudo apt update && \
+sudo apt install -y webmin
