3945bf5de609dca1232c177dbc311420e1be8141
teofilorf <teofilo@rabelo.net.br>
2017-05-15 15:27:17 -0300

    123341

--- mysql/instmysql.sh
+++ mysql/instmysql.sh
@@ -2,3 +2,6 @@
 
 sudo apt update
 sudo apt -y install mysql-server-5.7
+sudo apt -y install apache2
+sudo apt -y install php php-cgi libapache2-mod-php php-common php-pear php-mbstring
+sudo apt -y install phpmyadmin php-mbstring php-gettext
