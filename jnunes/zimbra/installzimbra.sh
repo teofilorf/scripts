9d5f5f65c3ae6035cd9de1586f5b17fc6a16f9fe
teofilorf <teofilo@rabelo.net.br>
2017-05-18 17:10:23 -0300

    123341

--- jnunes/zimbra/installzimbra.sh
+++ jnunes/zimbra/installzimbra.sh
@@ -3,6 +3,5 @@
 mkdir ~/Desktop
 wget -c https://files.zimbra.com/downloads/zdesktop/7.2.8/b12102/zdesktop_7_2_8_ga_b12102_20161005054617_linux_i686.tgz
 tar -zxvf zdesktop_7_2_8_ga_b12102_20161005054617_linux_i686.tgz
-cd zdesktop_7_2_8_ga_b12102_linux_i686
-cat config.txt | ./install.pl
+cat config.txt | zdesktop_7_2_8_ga_b12102_linux_i686/install.pl
 cp ~/Desktop/zd.desktop ~/Área\ de\ Trabalho/
