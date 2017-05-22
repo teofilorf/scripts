bafe2ef6d741d3701ea1cd0b0c20588e007d5d05
teofilorf <teofilo@rabelo.net.br>
2017-05-22 15:47:39 -0300

    123341

--- jnunes/files/grdesktop.sh
+++ jnunes/files/grdesktop.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-
-servidor=$(yad --title "Acesso Remoto" --text "Servidor" --entry)
+conf=~/grdesktop.conf
+if [ -e "$conf" ] ;then
+  texto=$(cat "$conf")
+else
+  >"$conf"
+fi
+servidor=$(yad --title "Acesso Remoto" --text "Servidor" --entry --entry-text="$texto")
+echo $servidor > $conf
 rdesktop -d jotanunes -g 1360x700 $servidor
