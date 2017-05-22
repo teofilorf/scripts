2893e0eda2b504d774e45265b6a30fa154c50d51
teofilorf <teofilo@rabelo.net.br>
2017-05-22 13:01:03 -0300

    123341

--- jnunes/zimbra/installzimbra.sh
+++ jnunes/zimbra/installzimbra.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-mkdir ~/Desktop
-wget -c https://files.zimbra.com/downloads/zdesktop/7.2.8/b12102/zdesktop_7_2_8_ga_b12102_20161005054617_linux_i686.tgz
-tar -zxvf zdesktop_7_2_8_ga_b12102_20161005054617_linux_i686.tgz
-cat config.txt | zdesktop_7_2_8_ga_b12102_linux_i686/install.pl
+sudo add-apt-repository -y ppa:webupd8team/java && sudo apt update && sudo apt install -y oracle-java8-set-default && \
+mkdir ~/Desktop && \
+wget -c https://s3.amazonaws.com/files.zimbra.com/downloads/zdesktop/7.3.0/b13060/zdesktop_7_3_0_ga_b13060_20170410030557_linux_x64.tgz && \
+tar -zxvf zdesktop_7_3_0_ga_b13060_20170410030557_linux_x64.tgz && \
+cat config.txt | zdesktop_7_3_0_ga_b13060_linux_x64/install.pl && \
 cp ~/Desktop/zd.desktop ~/Área\ de\ Trabalho/
 
